7d7f0b4480fab9d98bba22a046788dc786a5b363
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2025-03-20 12:49:12 +1300

    fix: stop looping back issue with pathfinding

--- pathing_nodes/scripts/monster_pathing.gd
+++ pathing_nodes/scripts/monster_pathing.gd
@@ -55,20 +55,37 @@ func calculate_path(destination: Node3D) -> Array[Node3D]:
 			print("Node has no neighbours womp womp!")
 			return []
 		
+		
+		# Starting the backtrack on a dead end
 		if (check_node.neighbour_nodes.size() == 1):
 			backtrack_path = true
-		elif (check_node.neighbour_nodes.size() >= 3):
-			backtrack_path = false
+		
+		var blocked_check_count: int = 0 
+		# Ending the backtrack on a dead end
+		while (blocked_check_count < check_node.neighbour_nodes.size() && backtrack_path):
+			if (!blocked_nodes.has(check_node.neighbour_nodes[blocked_check_count])):
+				backtrack_path = false
+				var first_index: int = path.find(prev_node)
+				if (first_index >= 0):
+					var second_index: int = path.find(prev_node, first_index)
+					print(first_index)
+					print(second_index)
+					# If index out of bounds array issues start happening its because of right under here
+					for i in range(first_index - 1, second_index + 2):
+						print(path)
+						path.remove_at((first_index - 1))
+						print(path)
+			blocked_check_count += 1
+		
 		
 		var best_index: int = -1
 		var best_distance: float = 69694206969; # Needs to be a big number and couldn't find a way to do the bit limit
 		for i in check_node.neighbour_nodes.size():
-			if (!blocked_nodes.has(check_node.neighbour_nodes[i]) || backtrack_path):
-				if (!prev_node == check_node.neighbour_nodes[i] || check_node.neighbour_nodes.size() == 1):
-					var distance: float = check_node.neighbour_nodes[i].position.distance_to(destination.position)
-					if (distance < best_distance):
-						best_distance = distance
-						best_index = i
+			if ((!blocked_nodes.has(check_node.neighbour_nodes[i]) || backtrack_path) && (!prev_node == check_node.neighbour_nodes[i] || check_node.neighbour_nodes.size() == 1)):
+				var distance: float = check_node.neighbour_nodes[i].position.distance_to(destination.position)
+				if (distance < best_distance):
+					best_distance = distance
+					best_index = i
 		
 		print(str(best_distance) + ", " + str(best_index))
 		
