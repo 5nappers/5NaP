670eb7ae5afc6c1b74376be417b8e7d5cdc1cc3b
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2025-03-20 09:27:02 +1300

    feat: setup pathing skeleton logic

--- pathing_nodes/scripts/monster_pathing.gd
+++ pathing_nodes/scripts/monster_pathing.gd
@@ -1,8 +1,9 @@
 extends Node3D
 
 @export var current_node: Node3D
-var goal_node: Node3D
+#var goal_node: Node3D
 var time: float = 0
+var path_to_goal: Array[Node] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,13 +14,35 @@ func _process(delta: float) -> void:
 	time += delta
 	var cooldown: float = 2
 	if time >= cooldown:
-		select_new_node()
+		move_to_next_node()
 		time = fmod(time, cooldown) 
 
-func select_new_node() -> void:
-	var neighbour_nodes: Array[Node] = current_node.neighbour_nodes
-	move_node(neighbour_nodes[0])
+func move_to_next_node() -> void:
+	if (path_to_goal.size() == 0): 
+		print("No Path For Monster (This may or may not be intended)")
+		return
+	
+	var new_node: Node3D = path_to_goal[0]
+	move_node(new_node)
+	path_to_goal.pop_front()
 
 func move_node(new_node: Node3D) -> void:
 	position = new_node.position
 	current_node = new_node
+	
+	# If arrived at end of path
+	if (path_to_goal[path_to_goal.size() - 1] == current_node):
+		arrived_at_destination()
+
+# This is called by whatever script (some state machine) that tells the freaks where to path to
+func set_new_destination(new_node: Node) -> void:
+	path_to_goal = calculate_path(new_node)
+
+func calculate_path(destination: Node) -> Array[Node]:
+	var path: Array[Node] = []
+	
+	
+	return path
+	
+func arrived_at_destination() -> void:
+	print("Arrived at path, this could call an event or something later");
