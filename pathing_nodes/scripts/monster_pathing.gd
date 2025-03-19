f5c873a75ddc79de2a5b1aa2a22340d75f23a428
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2025-03-20 08:43:24 +1300

    refactor: split move method into two

--- pathing_nodes/scripts/monster_pathing.gd
+++ pathing_nodes/scripts/monster_pathing.gd
@@ -1,6 +1,7 @@
-extends MeshInstance3D
+extends Node3D
 
-@export var current_node: Node
+@export var current_node: Node3D
+var goal_node: Node3D
 var time: float = 0
 
 # Called when the node enters the scene tree for the first time.
@@ -12,10 +13,13 @@ func _process(delta: float) -> void:
 	time += delta
 	var cooldown: float = 2
 	if time >= cooldown:
-		move_node();
+		select_new_node()
 		time = fmod(time, cooldown) 
-		
-func move_node() -> void:
+
+func select_new_node() -> void:
 	var neighbour_nodes: Array[Node] = current_node.neighbour_nodes
-	position = neighbour_nodes[0].position
-	current_node = neighbour_nodes[0]
+	move_node(neighbour_nodes[0])
+
+func move_node(new_node: Node3D) -> void:
+	position = new_node.position
+	current_node = new_node
