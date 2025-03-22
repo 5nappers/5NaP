106bdfe283212546dab5c878dd2a4f7537c15057
otherotherjosh <itsotherjosh@gmail.com>
2025-03-23 00:47:49 +1300

    feat: set edge zones as dictionary

--- player_camera.gd
+++ player_camera.gd
@@ -6,24 +6,36 @@ var edge_left := 0
 var edge_right := 0
 var edge_top := 0
 var edge_bottom := 0
+var edges := {
+	left = 0.0,
+	right = 0.0,
+	top = 0.0,
+	bottom = 0.0,
+}
 
 
 func _ready() -> void:
-	pass
+	set_edges()
+	
 	
 func _process(delta: float) -> void:
 	var mouse_position := get_viewport().get_mouse_position()
-	var resolution := get_viewport().get_visible_rect().size
-	print("resolution: %\n  position: %".format([resolution, mouse_position], "%"))
 	
 	# TODO: when the cursor is on the side of the screen, move camera to that side
 	# at an interpolated speed based on closeness to the boreder
 	# make sure to give bounds to how far left/right the player can look
-	if mouse_position < 
+	set_edges()
 	
 	# TODO: when the cursor is at the top, look up
 	# then when cursor goes to the bottom look back to normal
-	pass
+
 
 func set_edges() -> void:
+	var resolution := get_viewport().get_visible_rect().size
+
+	edges.left = edge_threshold_percentage * resolution.x / 100
+	edges.right = resolution.x - edges.left
+	edges.top = edge_threshold_percentage * resolution.y / 100
+	edges.bottom = resolution.y - edges.top
+	
 	edge_left = 
