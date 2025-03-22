0339b3cb73adc2c00b24122f46f47028830a79c0
otherotherjosh <itsotherjosh@gmail.com>
2025-03-22 23:21:42 +1300

    feat: set screen edge zones

--- player_camera.gd
+++ player_camera.gd
@@ -1,16 +1,29 @@
 extends Camera3D
 
-@export_range(0,200) var edge_width := 100
+@export_range(0,45) var edge_threshold_percentage := 30
+
+var edge_left := 0
+var edge_right := 0
+var edge_top := 0
+var edge_bottom := 0
 
 
 func _ready() -> void:
 	pass
 	
 func _process(delta: float) -> void:
+	var mouse_position := get_viewport().get_mouse_position()
+	var resolution := get_viewport().get_visible_rect().size
+	print("resolution: %\n  position: %".format([resolution, mouse_position], "%"))
+	
 	# TODO: when the cursor is on the side of the screen, move camera to that side
 	# at an interpolated speed based on closeness to the boreder
 	# make sure to give bounds to how far left/right the player can look
-	# 
+	if mouse_position < 
+	
 	# TODO: when the cursor is at the top, look up
 	# then when cursor goes to the bottom look back to normal
 	pass
+
+func set_edges() -> void:
+	edge_left = 
