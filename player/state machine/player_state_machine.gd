787c588165515f35bafd0ede972b8f2d72e9fec0
otherotherjosh <132322996+otherotherjosh@users.noreply.github.com>
2025-04-05 17:52:04 +1300

    feat(minigame): click on object to focus on it

--- player/state machine/player_state_machine.gd
+++ player/state machine/player_state_machine.gd
@@ -5,7 +5,6 @@ class_name PlayerStateMachine
 @export_range(0,45) var edge_threshold_percentage := 30
 
 @onready var look_around: PlayerState = $LookAround
-@onready var look_up: PlayerState = $LookUp
 @onready var focus: PlayerState = $Focus
 
 	
@@ -28,3 +27,10 @@ func get_active_state() -> PlayerState:
 func handle_player_input(value: Player.PlayerInput) -> void:
 	input = value
 	active_state.input = input
+
+
+func handle_focusable_clicked(focusable: Node3D) -> void:
+	if active_state != look_around:
+		return
+	focus.focused_object = focusable
+	active_state = focus
