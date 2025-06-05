e86ac5b1d367585434fe50f4592ff1e9269c9741
otherotherjosh <itsotherjosh@gmail.com>
2025-06-05 16:39:00 +1200

    feat(minigame): add flavor text on end screen

--- minigame/end_level.gd
+++ minigame/end_level.gd
@@ -4,6 +4,7 @@ extends Control
 
 
 var score: float
+var grade: String
 
 @onready var feedback_label: Label = $FeedbackLabel
 @onready var grade_label: Label = $GradeLabel
@@ -13,12 +14,13 @@ func _ready() -> void:
 	score = SceneLoader.current_score
 	var questions_correct := SceneLoader.current_questions_correct
 	var question_count = AssignmentDatabase.current_assignment.data.questions.size()
-	var grade := grade()
+	grade = calculate_grade()
 	grade_label.text = "Score: %s/%s\nGrade: %s" % [questions_correct, question_count, grade]
+	feedback_label.text = feedback()
 
 
 # returns a grade based on the score and NZ's grading scale
-func grade() -> String:
+func calculate_grade() -> String:
 	if score >= 0.9:
 		return "A+"
 	if score >= 0.85:
@@ -40,6 +42,23 @@ func grade() -> String:
 	if score >= 0.45:
 		return "D"
 	return "F"
+
+
+func feedback() -> String:
+	match grade:
+		"A+":
+			return "Incredible!!"
+		"A", "A-":
+			return "Great job!"
+		"B+", "B", "B-":
+			return "Good work!"
+		"C+", "C", "C-":
+			return "You did it!"
+		"D":
+			return "Nice effort"
+		"F":
+			return "You're better than this."
+	return "You tried."
 
 
 func _on_play_again_button_down() -> void:
