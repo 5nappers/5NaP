e2c2b51c5394c6d56d2f878eddb5530f1164eea7
Amber <amberj3lly@gmail.com>
2025-06-08 17:44:08 +1200

    feat(animations): Added animations along with walking to nodes not teleporting

--- pathing_nodes/scripts/pathing_controller.gd
+++ pathing_nodes/scripts/pathing_controller.gd
@@ -1,3 +1,4 @@
+# PathingController.gd
 class_name PathingController
 
 extends Node
@@ -5,47 +6,40 @@ extends Node
 @export var pathing: MonsterPathing
 @export var timer: Timer
 
-# Aggression is used to determine if the monster wants to path towards the player or away from them and will change over time.
-# Difficulty effects it, but difficulty on its own primarily effects how fast the monsters will path and is a static value per level.
 const max_aggression: float = 50
 const aggression_multiplier: float = 0.25
-const timer_time: float = 10;
+const timer_time: float = 10
 
 var aggression: float = 0
-var isAttacking: bool = false;
-
+var isAttacking: bool = false
 
 func _ready() -> void:
-	pathing.difficulty = clamp(pathing.difficulty, 1, pathing.max_difficulty)	
+	pathing.difficulty = clamp(pathing.difficulty, 1, pathing.max_difficulty)
 	timer.timeout.connect(on_timer_timeout)
 	pathing.finish_path.connect(on_finish_path)
 	timer.start(timer_time)
-
 
 func _process(delta: float) -> void:
 	aggression += ((pathing.difficulty as float / pathing.max_difficulty) + 5 * aggression_multiplier) * delta * (pathing.difficulty * aggression_multiplier)
 	aggression = clampf(aggression, 0, max_aggression)
 
-
 func on_timer_timeout() -> void:
 	if (pathing.is_movement_paused):
-		pathing.is_movement_paused = false;
-	
+		pathing.is_movement_paused = false
 	check_agression_direction()
 	change_to_logical_path()
+	pathing.move_to_next_node()
 	timer.start(timer_time)
-
 
 func on_finish_path() -> void:
 	aggression = 0
 	change_to_logical_path()
-	pathing.is_movement_paused = true;
+	pathing.is_movement_paused = true
 	timer.stop()
-	timer.start(10) # how long the monster waits after capturing you, can be set by animation or something later
+	timer.start(10)
 
 func check_agression_direction() -> void:
 	isAttacking = aggression == max_aggression
-
 
 func change_to_logical_path() -> void:
 	if (isAttacking && !pathing.path_completed):
